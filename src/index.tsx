96ab227b3d1f52784df8c68ca7f3a3b186e13222
Carlos Roberto <carlos.junior@dotgroup.com.br>
2023-03-29 11:10:34 -0300

    change ga library to GA4

--- src/index.tsx
+++ src/index.tsx
@@ -1,12 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import ReactGA from 'react-ga';
+import ReactGA from 'react-ga4';
 import App from './App';
 
-ReactGA.initialize('G-TGW6ESXKH9', { debug: false });
-ReactGA.pageview(window.location.pathname + window.location.search);
-
-// import reportWebVitals from './reportWebVitals';
+ReactGA.initialize('G-TGW6ESXKH9');
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement,
@@ -17,5 +14,3 @@ root.render(
     <App />
   </React.StrictMode>,
 );
-
-// reportWebVitals();

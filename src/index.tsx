e78b63a79f1c78bbfa98bf744cb5c6ad46182812
Carlos Roberto <carlos.junior@dotgroup.com.br>
2023-03-28 22:09:47 -0300

    add react-ga

--- src/index.tsx
+++ src/index.tsx
@@ -1,12 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import ReactGA from 'react-ga';
 import App from './App';
+
+ReactGA.initialize('G-TGW6ESXKH9', { debug: false });
 
 // import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement,
 );
+
 root.render(
   <React.StrictMode>
     <App />

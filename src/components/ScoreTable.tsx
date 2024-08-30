78604d80cc9f96438a776159475899c7176fe2f8
Carlos Júnior <crpu4a@gmail.com>
2024-08-30 10:54:17 -0300

    feat: change time format

--- src/components/ScoreTable.tsx
+++ src/components/ScoreTable.tsx
@@ -44,12 +44,12 @@ const ScoreTable = ({
             ))}
           {!!totalMinutes(extraHour) && (
             <tr className="bg-green-100">
-              <td className="td-table dark:text-slate-700" title={`Hora Extra`}>
-                Extras:
+              <td className="td-table dark:text-slate-700" title={`Banco de Horas`}>
+                Banco:
               </td>
               <td className="td-table"> </td>
-              <td className="td-table dark:text-slate-700" title={`Hora Extra`}>
-                {extraHour}
+              <td className="td-table dark:text-slate-700" title={`Banco de Horas`}>
+                { formatMinutesToHourString(totalMinutes(extraHour))}
               </td>
             </tr>
           )}
